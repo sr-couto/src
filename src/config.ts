a7c23886804431d75e5302696231722b68063d09
sr-couto <santuan.web@gmail.com>
2024-02-06 17:26:32 -0300

    config

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'SRC';
-export const SITE_DESCRIPTION = 'En este espacio encontrará algunos textos, historias y recursos que se relacionan de alguna manera con el mundo del diseño, la programación y el software libre.';
+export const SITE_DESCRIPTION = 'En este espacio encontrará algunos textos, historias y recursos que se relacionan de alguna manera con el mundo.';
